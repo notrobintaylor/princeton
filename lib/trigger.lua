47a12d210a2532809d264033f44daf0250a2985f
notrobintaylor <49676208+notrobintaylor@users.noreply.github.com>
2026-06-29 19:31:35 +0200

    Add files via upload
    v1.4.1

--- lib/trigger.lua
+++ lib/trigger.lua
@@ -22,8 +22,6 @@ trigs.target_param_filter  = {}
 
 for i = 1, trigs.N do trigs.last_global[i] = 1 end
 
-local NUM_LFOS = 8
-
 local looper, lfo
 local is_initing, is_clock_running
 local binding
@@ -76,53 +74,6 @@ function trigs.strip_fn.advance(idx, current, d)
     next_idx = next_idx + step
   end
   return current
-end
-
-trigs.TARGETS = {
-  { label = "Off" },
-  { label = "Push: Toggle",    id = "trig_push_toggle",    action = function() params:set("push_enable",    3 - params:get("push_enable"))    end },
-  { label = "Distort: Toggle", id = "trig_distort_toggle", action = function() params:set("distort_enable", 3 - params:get("distort_enable")) end },
-  { label = "Warp: Toggle",    id = "trig_warp_toggle",    action = function() params:set("warp_enable",    3 - params:get("warp_enable"))    end },
-  { label = "Repeat: Toggle",  id = "trig_repeat_toggle",  action = function() params:set("repeat_enable",  3 - params:get("repeat_enable"))  end },
-  { label = "Amp: Toggle",     id = "trig_amp_toggle",     action = function() params:set("amp_enable",     3 - params:get("amp_enable"))     end },
-  { label = "Tremolo: Toggle", id = "trig_tremolo_toggle", action = function() params:set("tremolo_enable", 3 - params:get("tremolo_enable")) end },
-  { label = "Looper: Rec",     id = "trig_looper_rec",     action = function() looper.step() end },
-  { label = "Looper: Clear",   id = "trig_looper_clear",   action = function() looper.force_clear() end },
-  { label = "Reverb: Toggle",  id = "trig_reverb_toggle",  action = function() params:set("reverb_enable",  3 - params:get("reverb_enable"))  end },
-  { label = "Limit: Toggle",   id = "trig_limit_toggle",   action = function() params:set("limit_enable",   3 - params:get("limit_enable"))   end },
-}
-for i = 1, NUM_LFOS do
-  trigs.TARGETS[#trigs.TARGETS+1] = {
-    label = "LFO "..i..": Randomize", id = "trig_lfo"..i.."_randomize", lfo_idx = i,
-    action = function() params:set("lfo"..i.."_randomize", 1) end
-  }
-end
-
-trigs.DEVICES, trigs.DEVICE_PARAMS = (function()
-  local devices = { "-" }
-  local dparams = { [1] = { { label = "-", global_idx = 1 } } }
-  for i = 2, #trigs.TARGETS do
-    local t = trigs.TARGETS[i]
-    local dev_name, short_name = t.label:match("^(.-): (.+)$")
-    if dev_name then
-      local di
-      for ix, dn in ipairs(devices) do
-        if dn == dev_name then di = ix; break end
-      end
-      if not di then
-        devices[#devices + 1] = dev_name
-        di = #devices
-        dparams[di] = {}
-      end
-      dparams[di][#dparams[di] + 1] = { label = short_name, global_idx = i }
-    end
-  end
-  return devices, dparams
-end)()
-
-trigs.TARGET_DEVICE_OF = {}
-for di, plist in pairs(trigs.DEVICE_PARAMS) do
-  for _, e in ipairs(plist) do trigs.TARGET_DEVICE_OF[e.global_idx] = di end
 end
 
 function trigs.fn.refresh_suspend(idx)
@@ -212,6 +163,35 @@ function trigs.init(deps)
   lfo              = deps.lfo
   is_initing      = deps.is_initing
   is_clock_running = deps.is_clock_running
+
+  trigs.TARGETS = deps.targets
+
+  trigs.DEVICES, trigs.DEVICE_PARAMS = (function()
+    local devices = { "-" }
+    local dparams = { [1] = { { label = "-", global_idx = 1 } } }
+    for i = 2, #trigs.TARGETS do
+      local t = trigs.TARGETS[i]
+      local dev_name, short_name = t.label:match("^(.-): (.+)$")
+      if dev_name then
+        local di
+        for ix, dn in ipairs(devices) do
+          if dn == dev_name then di = ix; break end
+        end
+        if not di then
+          devices[#devices + 1] = dev_name
+          di = #devices
+          dparams[di] = {}
+        end
+        dparams[di][#dparams[di] + 1] = { label = short_name, global_idx = i }
+      end
+    end
+    return devices, dparams
+  end)()
+
+  trigs.TARGET_DEVICE_OF = {}
+  for di, plist in pairs(trigs.DEVICE_PARAMS) do
+    for _, e in ipairs(plist) do trigs.TARGET_DEVICE_OF[e.global_idx] = di end
+  end
 
   for i = 1, trigs.N do
     trigs.target_device_filter[i] = {}
